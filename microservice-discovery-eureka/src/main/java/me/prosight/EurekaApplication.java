7bf28657d22aa1091d88a3666b24d6b865ec03ca
Elton Zheng <elton.zheng@me.com>
2017-02-05 12:35:01 +0800

    实现eureka服务

---
+++ microservice-discovery-eureka/src/main/java/me/prosight/EurekaApplication.java
@@ -0,0 +1,18 @@
+package me.prosight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by elton on 2017/2/5.
+ * Eureka 服务器
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
