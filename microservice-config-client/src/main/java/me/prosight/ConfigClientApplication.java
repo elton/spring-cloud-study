41b0fb12f43fd334c783fbc40e3460765d470428
Elton Zheng <elton.zheng@me.com>
2017-02-06 23:06:47 +0800

    添加config-client

---
+++ microservice-config-client/src/main/java/me/prosight/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package me.prosight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by elton on 2017/2/6.
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
