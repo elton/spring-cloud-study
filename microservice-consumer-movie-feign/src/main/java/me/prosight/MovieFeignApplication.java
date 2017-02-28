3e611ea5aafafaeeeec8c81f42eca029175d95bb
Elton Zheng <elton.zheng@me.com>
2017-02-28 22:57:35 +0800

    add feign

--- microservice-consumer-movie-feign/src/main/java/me/prosight/MovieFeignApplication.java
+++ microservice-consumer-movie-feign/src/main/java/me/prosight/MovieFeignApplication.java
@@ -3,16 +3,18 @@ package me.prosight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
- * Created by elton on 2017/2/5.
- * 通过@EnableDiscoveryClient注解，即可将microservice-provider-user服务注册到Eureka上面去
+ * Created by elton on 2017/2/28.
+ * 使用@EnableFeignClients开启Feign
  */
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableDiscoveryClient
-public class UserApplication {
+public class MovieFeignApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(MovieFeignApplication.class, args);
     }
 }
---
+++ microservice-consumer-movie-feign/src/main/java/me/prosight/controller/FeignController.java
@@ -0,0 +1,25 @@
+package me.prosight.controller;
+
+
+import me.prosight.feign.UserFeignClient;
+import me.prosight.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by elton on 2017/2/28.
+ *
+ */
+@RestController
+public class FeignController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("feign/{id}")
+    public User findByIdFeign(@PathVariable Long id) {
+        User user = this.userFeignClient.findByIdFeign(id);
+        return user;
+    }
+}
